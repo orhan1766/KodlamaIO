441068713df9777154eb24fd3053e7384e147454
orhan1766 <orhan1766@gmail.com>
2024-02-16 01:04:18 +0300

    Add project files.

---
+++ DataAccess/Concrete/InstructorDal.cs
@@ -0,0 +1,47 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class InstructorDal : IInstructorDal
+    {
+        private List<Instructor> _instructors;
+        public InstructorDal()
+        {
+            Instructor instructor1 = new Instructor { Id=1, Name="Engin Demiroğ", About="Adam!"};
+            Instructor instructor2 = new Instructor { Id = 2, Name = "Atıl Samancıoğlu", About = "Başka  bir Adam!" };
+
+            List<Instructor> instructors = new List<Instructor> { instructor1, instructor2};
+            _instructors = instructors;
+        }
+        public void Add(Instructor entity)
+        {
+            _instructors.Add(entity);
+        }
+
+        public void Delete(Instructor entity)
+        {
+            _instructors.Remove(entity);
+        }
+
+        public Instructor Get(int id)
+        {
+            return _instructors.Where(i=>i.Id==id).FirstOrDefault();
+        }
+
+        public List<Instructor> GetAll()
+        {
+            return _instructors;  
+        }
+
+        public void Update(Instructor entity)
+        {
+            _instructors.Where(i => i.Id == entity.Id).FirstOrDefault().Name = entity.Name;
+        }
+    }
+}
---
+++ Entities/Concrete/Course.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Course:IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Process { get; set; }
+        public string CategoryName { get; set; }
+        public string InstructorName { get; set; }
+
+    }
+}
